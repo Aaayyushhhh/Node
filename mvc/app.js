9e6e22a2ecd7eeba1b7ec3519a06b7361a7c41e2
Aayush Rana <aayushrana@Aayushs-MacBook-Air.local>
2025-11-07 09:58:49 +0530

    7/11/2025

--- mvc/app.js
+++ mvc/app.js
@@ -5,10 +5,10 @@ const path = require('path');
 const express = require('express');
 
 //Local Module
-const userRouter = require("./routes/userRouter")
-const {hostRouter} = require("./routes/hostRouter")
+const storeRouter = require("./routes/storeRouter")
+const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
-const errorsController = require("./controllers/errors")
+const errorsController = require("./controllers/errors");
 
 const app = express();
 
@@ -16,14 +16,14 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded());
-app.use(userRouter);
+app.use(storeRouter);
 app.use("/host", hostRouter);
 
 app.use(express.static(path.join(rootDir, 'public')))
 
 app.use(errorsController.pageNotFound);
 
-const PORT = 3021;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
