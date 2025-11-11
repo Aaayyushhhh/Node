4220821af15b5a5d49bc7bf6f14147ec2c4f7612
Aayush Rana <aayushrana@Aayushs-MacBook-Air.local>
2025-11-11 20:55:40 +0530

    11/11/2025

--- dynamic-Paths/controllers/storeController.js
+++ dynamic-Paths/controllers/storeController.js
@@ -1,75 +1,78 @@
 const Home = require("../models/home");
-
+const Favourite = require("../models/favourite");
 
 exports.getIndex = (req, res, next) => {
-  Home.fetchAll((registeredHomes) =>
+  Home.fetchAll((registeredHomes) => {
     res.render("store/index", {
       registeredHomes: registeredHomes,
       pageTitle: "airbnb Home",
       currentPage: "index",
-    })
-  );
+    });
+  });
 };
 
 exports.getHomes = (req, res, next) => {
-  Home.fetchAll((registeredHomes) =>
+  Home.fetchAll((registeredHomes) => {
     res.render("store/home-list", {
       registeredHomes: registeredHomes,
       pageTitle: "Homes List",
       currentPage: "Home",
-    })
-  );
+    });
+  });
 };
 
 exports.getBookings = (req, res, next) => {
   res.render("store/bookings", {
     pageTitle: "My Bookings",
     currentPage: "bookings",
-  })
+  });
 };
 
-
 exports.getFavouriteList = (req, res, next) => {
-  Home.fetchAll((registeredHomes) =>
-    res.render("store/favourite-list", {
-      registeredHomes: registeredHomes,
-      pageTitle: "My Favourites",
-      currentPage: "favourites",
-      }));
-    };
+  Favourite.getFavourites(favourites => {
+    Home.fetchAll((registeredHomes) => {
+      const favouriteHomes = registeredHomes.filter(home => favourites.includes(home.id));
+      res.render("store/favourite-list", {
+        favouriteHomes: favouriteHomes,
+        pageTitle: "My Favourites",
+        currentPage: "favourites",
+      })
+    });
+  })
+
+};
 
 exports.postAddToFavourite = (req, res, next) => {
-  Favourite.addToFavourite(req.body.id, error => {
+  Favourite.addToFavourite(req.body.id, (error) => {
     if (error) {
-      console.log("Error while marking favourite: ", error);
+      console.log("Error while marking favourite:", error);
     }
     res.redirect("/favourites");
-  })
-}
+  });
+};
 
 exports.postRemoveFromFavourite = (req, res, next) => {
   const homeId = req.params.homeId;
-  Favourite.deleteById(homeId, error => {
+  Favourite.deleteById(homeId, (error) => {
     if (error) {
-      console.log('Error while removing from Favourite', error);
+      console.log("Error while removing from Favourite", error);
     }
     res.redirect("/favourites");
-  })
-}
+  });
+};
 
 exports.getHomeDetails = (req, res, next) => {
   const homeId = req.params.homeId;
-  Home.findById(homeId,home =>{
-    if(!home){
-      console.log("Home not found")
-      res.redirect("/homes")
-    }else{
-  res.render("store/home-detail", {
-    home:home,
-    pageTitle: "Home Detail",
-    currentPage: "Home",
-     });
+  Home.findById(homeId, (home) => {
+    if (!home) {
+      console.log("Home not found");
+      res.redirect("/homes");
+    } else {
+      res.render("store/home-detail", {
+        home: home,
+        pageTitle: "Home Detail",
+        currentPage: "Home",
+      });
     }
-  })
+  });
 };
-
